dd1e53dbb700691576fedf4323dfd17e38e4e76d
David bao <davidbao@netease.com>
2014-01-20 22:38:22 +0800

    modify some code of ui.

--- ActivateCPOS/ViewController.h
+++ ActivateCPOS/ViewController.h
@@ -8,10 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UITableViewController
+@interface ViewController : UITableViewController{
+    int _currentTime;  // unit: ms
+    int _clickCount;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *textStationId;
+@property (weak, nonatomic) IBOutlet UITextField *textCode1;
+@property (weak, nonatomic) IBOutlet UITextField *textCode2;
+@property (weak, nonatomic) IBOutlet UITextField *textCode3;
+@property (weak, nonatomic) IBOutlet UITextField *textCode4;
+@property (weak, nonatomic) IBOutlet UITextField *textActivationCode;
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string;
 
-@property (weak, nonatomic) IBOutlet UITextField *textStationId;
+- (IBAction)textStationIdChanged:(UITextField *)sender;
+
+- (IBAction)textCode1Changed:(UITextField *)sender;
+- (IBAction)textCode2Changed:(UITextField *)sender;
+- (IBAction)textCode3Changed:(UITextField *)sender;
+
+- (IBAction)activationClick:(UIButton *)sender;
 
 @end
--- ActivateCPOS/ViewController.m
+++ ActivateCPOS/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 #define NUMBERS_ONLY @"1234567890"
-#define CHARACTER_LIMIT 8
+#define CHARACTER_LIMIT8 8
+#define CHARACTER_LIMIT5 5
+
+#define MaxClickCount 3
 
 @interface ViewController ()
 
@@ -22,7 +25,15 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    _textStationId.text = @"test";
+    _textStationId.delegate = (id)self;
+    _textCode1.delegate = (id)self;
+    _textCode2.delegate = (id)self;
+    _textCode3.delegate = (id)self;
+    _textCode4.delegate = (id)self;
+    
+    //The setup code (in viewDidLoad in your view controller)
+    UITapGestureRecognizer *singleFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
+    [self.view addGestureRecognizer:singleFingerTap];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,7 +46,90 @@
     NSUInteger newLength = [textField.text length] + [string length] - range.length;
     NSCharacterSet *cs = [[NSCharacterSet characterSetWithCharactersInString:NUMBERS_ONLY] invertedSet];
     NSString *filtered = [[string componentsSeparatedByCharactersInSet:cs] componentsJoinedByString:@""];
-    return (([string isEqualToString:filtered])&&(newLength <= CHARACTER_LIMIT));
+
+    int limit = textField == _textStationId ? CHARACTER_LIMIT8 : CHARACTER_LIMIT5;
+    return (([string isEqualToString:filtered])&&(newLength <= limit));
+}
+
+- (IBAction)textStationIdChanged:(UITextField *)sender {
+    if ([_textStationId.text length] == CHARACTER_LIMIT8) {
+        [_textCode1 becomeFirstResponder];
+    }
+}
+
+- (IBAction)textCode1Changed:(UITextField *)sender {
+    if ([_textCode1.text length] == CHARACTER_LIMIT5) {
+        [_textCode2 becomeFirstResponder];
+    }
+}
+
+- (IBAction)textCode2Changed:(UITextField *)sender {
+    if ([_textCode2.text length] == CHARACTER_LIMIT5) {
+        [_textCode3 becomeFirstResponder];
+    }
+}
+
+- (IBAction)textCode3Changed:(UITextField *)sender {
+    if ([_textCode3.text length] == CHARACTER_LIMIT5) {
+        [_textCode4 becomeFirstResponder];
+    }
+}
+
+//The event handling method
+- (void)handleSingleTap:(UITapGestureRecognizer *)recognizer {
+    long time = [[NSDate date] timeIntervalSince1970];
+    if(_currentTime == 0 || time - _currentTime < 1000){
+        _clickCount++;
+    }
+    else{
+        _clickCount = 0;
+    }
+    _currentTime = time;
+    
+    if(_clickCount > MaxClickCount){
+        _clickCount = 0;
+        _currentTime = 0;
+    }
+}
+
+- (IBAction)activationClick:(UIButton *)sender {
+    Boolean ignoreStationId = _clickCount >= MaxClickCount;
+    _clickCount = 0;
+    _currentTime = 0;
+    if (ignoreStationId) {
+    }
+    
+//    NSURL *url = [NSURL URLWithString:@"http://northloong.com:8733/ActivateService.asmx"];
+//    ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
+//    [request setRequestMethod:@"POST"];
+//    [request addRequestHeader:@"Content-Type" value:@"text/xml; charset=utf-8"];
+//    [request addRequestHeader:@"SOAPAction" value:@"http://tempuri.org/HelloWorld"];
+//    
+//    
+//    NSString *soapMessage = [NSString stringWithFormat:
+//                             @"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+//                             
+//                             "<soap:Envelope
+//                                                 xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+//                                                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"
+//                                                 xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+//                             "<soap:Body>"
+//                             "<Greet xmlns=\"http://tempuri.org/\">"
+//                             "<deviceToken>some device token</deviceToken>"
+//                             "<userName>azamsharp</userName>"
+//                             "</Greet>"
+//                             "</soap:Body>"
+//                             "</soap:Envelope>"];
+//    
+//    NSString *messageLength = [NSString stringWithFormat:@"%d",[soapMessage length]];
+//    
+//    [request addRequestHeader:@"Content-Length" value:messageLength];
+//    
+//    [request appendPostData:[soapMessage dataUsingEncoding:NSUTF8StringEncoding]];
+//    
+//    [request setDelegate:self]; 
+//    [request startAsynchronous];
+   
 }
 
 @end
